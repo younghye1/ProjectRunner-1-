33c2a3508e1930cefae411865ee2437638aefb94
younghye1 <265o21247050@gmail.com>
2025-03-07 10:06:16 +0900

    0.1.12]진짜끝

--- Assets/_Scripts/Scenes/UIManager.cs
+++ Assets/_Scripts/Scenes/UIManager.cs
@@ -1,8 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField]TextMeshProUGUI tmversion;
+
+    void OnValidate()
+    {
+        if ( tmversion != null)
+        tmversion.text = $"v{Application.version}";
+    }
+
     public void OnStartButtonClick()
     {
         SceneManager.LoadScene("1Ingame"); // "1Ingame" 씬으로 이동
